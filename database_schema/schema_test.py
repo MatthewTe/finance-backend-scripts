ff99e3fee5e95f7eadafb99fa2604ea6f66b9616
Matthew Teelucksingh <47713353+MatthewTe@users.noreply.github.com>
2022-12-26 04:24:18 +0000

    Added first skeleton for database schema

--- database_schema/schema_test.py
+++ database_schema/schema_test.py
@@ -1,2 +1,21 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, Table, Column, String, Boolean, Integer
+
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
+
+# Main database Metadata object:
+metadata_obj = MetaData()
+
+company_table = Table(
+    "main_company_tbl",
+    metadata_obj,
+
+    Column("id", String, primay_key=True),
+    Column("name", String),
+    Column("ticker", String),
+    Column("public_status", Boolean),
+    Column("cik", Integer),
+    Column("home_country", String)
+
+)
+
+metadata_obj.create_all(engine)
